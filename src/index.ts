05c31471759db5c7c6b2a006174a6de1b9674f44
Victor Codonho <victor_codonho@outlook.com>
2023-12-14 10:15:43 -0300

    fix: debugg

--- src/index.ts
+++ src/index.ts
@@ -23,52 +23,135 @@ app.get('/api/status', (req: Request, res: Response) => {
 });
 
 app.get("/api/assessments", async (req: Request, res: Response) => {
-    const assessments = await prisma.assessment.findMany();
-    res.status(200).json(assessments);
+    try {
+        const assessments = await prisma.assessment.findMany();
+        res.status(200).json(assessments);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
 });
 
 app.get("/api/students", async (req: Request, res: Response) => {
-    const students = await prisma.student.findMany();
-    res.status(200).json(students);
+    try {
+        const students = await prisma.student.findMany();
+        res.status(200).json(students);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
 });
 
 app.get("/api/classes", async (req: Request, res: Response) => {
-    const classes = await prisma.classe.findMany();
-    res.status(200).json(classes);
+    try {
+        const classes = await prisma.classe.findMany();
+        res.status(200).json(classes);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
 });
 
 app.get("/api/matters", async (req: Request, res: Response) => {
-    const matters = await prisma.matter.findMany();
-    res.status(200).json(matters);
+    try {
+        const matters = await prisma.matter.findMany();
+        res.status(200).json(matters);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
 });
 
 app.get("/api/quiz/result", async (req: Request<null, null, null, QuizResultQuery>, res: Response) => {
+    try {
+        const query = req.query;
 
-    const query = req.query;
-
-    const quiz = await prisma.quiz.findMany({
-        where: {
-            studentId: Number(query.studentId),
-            assessmentId: Number(query.assessmentId),
-            Question: {
-                matterId: Number(query.matterId)
-            },
-            Student: {
-                classeId: Number(query.classeId)
-            }
-        },
-        include: {
-            Student: true,
-            Question: {
-                include: {
-                    Matter: true
+        const quiz = await prisma.quiz.findMany({
+            where: {
+                studentId: Number(query.studentId),
+                assessmentId: Number(query.assessmentId),
+                Question: {
+                    matterId: Number(query.matterId)
+                },
+                Student: {
+                    classeId: Number(query.classeId)
                 }
             },
-            Assessment: true
-        }
-    });
+            include: {
+                Student: true,
+                Question: {
+                    include: {
+                        Matter: true
+                    }
+                },
+                Assessment: true
+            }
+        });
 
-    res.status(200).json(quiz);
+        res.status(200).json(quiz);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
+});
+
+app.get("/api/quiz/recommendation", async (req: Request<null, null, null, QuizResultQuery>, res: Response) => {
+    try {
+        const query = req.query;
+
+        const quiz = await prisma.quiz.findMany({
+            where: {
+                studentId: Number(query.studentId),
+                assessmentId: Number(query.assessmentId),
+                Question: {
+                    matterId: Number(query.matterId)
+                },
+                Student: {
+                    classeId: Number(query.classeId)
+                }
+            },
+            include: {
+                Student: true,
+                Question: {
+                    include: {
+                        Matter: true
+                    }
+                },
+                Assessment: true
+            }
+        });
+
+        const prompt = `
+        Matéria | Questão | Nível | Resultado
+        ${quiz.map(question => {
+            return `${question.Question.Matter.name} | ${question.Question.name} | ${question.Question.level} | ${question.result ? "Acertou" : "Errou"}`
+        })
+            }
+
+        Analisando a tabela acima, o aluno realizou uma prova referente a matéria ${quiz[0].Question.Matter.name}, você poderia recomendar estudos para aluno melhorar os erros que ele cometeu na prova.
+    `;
+
+        const recommendation = await openai.chat.completions.create({
+            messages: [
+                {
+                    role: 'user',
+                    content: prompt
+                }
+            ],
+            model: 'gpt-3.5-turbo'
+        });
+
+        res.status(200).json(recommendation);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json(error);
+        }
+    }
 });
 
 app.listen(port, () => {
